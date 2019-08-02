d9c56c471823b8c5f08c3f139b25ea3f2420d61c
李锐 <328488656@qq.com>
2019-08-03 00:06:27 +0800

    easy C compiler

---
+++ text.cpp
@@ -0,0 +1,31 @@
+int  main(int g)
+{
+	int   a = 11;
+	int   d = 2;
+	int   i;
+	int   total=0;
+	int   q;
+	float r;
+	int   s;
+	int   lo;
+	char  y = 'a';
+	float k=1;
+	cin >> i;
+	r = 1 + 1.0;
+	s = a*d;
+	lo = a / d;
+	if(a > d) {
+		d = a + 1;
+	}
+	else{
+		a=d;
+	}
+	cout << total;
+	while(i<100){
+	    total=total+i;
+		i=i+1;
+	}
+	cout << total;
+	i=0;
+	return 0;
+}
